b9dc3ec2936669c4f3660dd95210d01e6c76b85a
Daniil Bastrich <bastrich@bk.ru>
2013-05-01 16:53:11 +0400

    new

---
+++ WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+using NAudio.Mixer;
+
+namespace WindowsFormsApplication1
+{
+    class Record
+    {
+
+        WaveIn waveIn;
+        private WaveFileWriter writer;
+
+
+        public Record()
+        {
+            waveIn = new WaveIn();
+            waveIn.WaveFormat = new WaveFormat(44100, 1);
+
+            writer = new WaveFileWriter("C:\test.wav", waveIn.WaveFormat);
+
+            waveIn.DataAvailable += OnDataAvailable;
+            waveIn.RecordingStopped += OnRecordingStopped;
+            waveIn.StartRecording();
+
+        }
+
+
+
+    }
+}
